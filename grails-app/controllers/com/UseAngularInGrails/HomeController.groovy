e87d667d3e205f09c7e87fec69305273ff3ba154
Mohit Virmani <mohit.virmani123@gmail.com>
2014-08-11 15:51:56 +0530

    Removed println , replaced by log.debug

--- grails-app/controllers/com/UseAngularInGrails/HomeController.groovy
+++ grails-app/controllers/com/UseAngularInGrails/HomeController.groovy
@@ -8,25 +8,25 @@ class HomeController {
 	}
 
 	def getAllNews(){
-		println "List"
+		log.debug "getAllNews started"
 		def res = new HashMap()
 		def news = News.list();
-		println news
+		log.debug "news " + news
 		if(news.size()>0){
 			res.news =news
 			res.status="success"
 			res.message="News list coming"
 		}else{
-		res.status="fails"
-		res.message="No News list are coming"
+			res.status="fails"
+			res.message="No News list are coming"
 		}
-		
+
 		respond res,[formats:['json', 'xml']];
 		return res
 	}
 
 	def saveNews(){
-		println "Add paramss "+params
+		log.debug "Add paramss "+params
 		def res = new HashMap()
 		if(params){
 			News obj = new News();
@@ -34,22 +34,21 @@ class HomeController {
 				obj.heading=params?.Heading
 			if(params?.Discription)
 				obj.descripton=params?.Discription
-				def uploadfile =  request.getFile("photo")
-				def webRootDir = servletContext.getRealPath("/")
-				println uploadfile.originalFilename +"==="+webRootDir
+			def uploadfile =  request.getFile("photo")
+			def webRootDir = servletContext.getRealPath("/")
+			log.debug uploadfile.originalFilename +"==="+webRootDir
 			if(uploadfile.originalFilename){
 				obj.pic = uploadfile.originalFilename
 				if(obj.save(flush:true)){
 					def path = 	webRootDir+"images"+File.separator+"newsImages"+File.separator+obj.id
 					File f1 = new File((path))
-					if(f1.exists())
-					{
-						println "exist== "+f1.exists()
+					if(f1.exists()) {
+						log.debug "exist== "+f1.exists()
 					}else{
-						println "created "+f1.mkdirs();
+						log.debug "created "+f1.mkdirs();
 					}
 					uploadfile.transferTo( new File( path, uploadfile.originalFilename))
-				    File f = new File((path+File.separator+uploadfile.originalFilename))
+					File f = new File((path+File.separator+uploadfile.originalFilename))
 					if(f.exists()) {
 						obj.picpath = "images"+File.separator+"newsImages"+File.separator+obj.id+File.separator+uploadfile.originalFilename
 						obj.save(flush:true)
@@ -60,7 +59,7 @@ class HomeController {
 						res.message="News Successfully Saved"
 					}
 				} else{
-				    obj.errors.each {print it}
+					obj.errors.each {print it}
 					res.status="success"
 					res.message="News Successfully Saved"
 				}
@@ -70,13 +69,12 @@ class HomeController {
 		respond res,[formats:['json', 'xml']];
 		return res
 	}
-	
-	
+
+
 	def renderImage(){
 		log.debug "render image started"+params.id
 		def news=News.findById(params.id);
-		if(new File((news.picpath)).exists())
-		{
+		if(new File((news.picpath)).exists()) {
 			File imageFile=new File(news.picpath)
 			byte[] buffer=new FileInputStream(imageFile).getBytes()
 			response.setContentLength(buffer.length)
