370c49b8c23ecc33961c4c501f443e2362cfb84b
Mohit Virmani <mohit.virmani@oodlestechnologies.com>
2014-08-26 12:19:35 +0530

    Updated frontPageDataServices

--- web-app/js/angularServices/frontPageDataService.js
+++ web-app/js/angularServices/frontPageDataService.js
@@ -1,35 +1,34 @@
 //Creates reference of useAngular module
-var homePage= angular.module('useAngular');
+var homePage = angular.module('useAngular');
 
 homePage.service('newsServices', function($http) {
 
-	  var newsListAPI = [];
-	  newsListAPI.getAllNews = function() {
-	      return $http({
-	        method: 'GET', 
-	        url: 'home/getAllNews'
-	      });
-	    }
-	    return newsListAPI;
+	var newsListAPI = [];
+	newsListAPI.getAllNews = function() {
+		return $http({
+			method : 'GET',
+			url : 'home/getAllNews'
+		});
+	}
+	return newsListAPI;
 });
-
 
 homePage.service('recipiesServices', function($http) {
 
-	  var recipieAPI = [];
-	  recipieAPI.getAllRecipies = function() {
-	      return $http({
-	        method: 'GET', 
-	        url: 'recipies/recipieslist'
-	      });
-	    }
-	  
-	  recipieAPI.deleteRecipie = function(id) {
-		  console.log("Id of Recipie "+id)
-	      return $http({
-	        method: 'POST', 
-	        url: 'recipies/deleteRecipie?id='+id
-	      });
-	    }
-	    return recipieAPI;
+	var recipieAPI = [];
+	recipieAPI.getAllRecipies = function() {
+		return $http({
+			method : 'GET',
+			url : 'recipies/recipieslist'
+		});
+	}
+
+	recipieAPI.deleteRecipie = function(id) {
+		console.log("Id of Recipie " + id)
+		return $http({
+			method : 'POST',
+			url : 'recipies/deleteRecipie?id=' + id
+		});
+	}
+	return recipieAPI;
 });
