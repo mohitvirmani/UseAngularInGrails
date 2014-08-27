151ac1593df8eed3583a13288b6ddf01506e7fe7
Deepak Gupta <deepak.gupta@oodlestechnologies.com>
2014-08-27 10:05:29 +0530

    add scroll in div

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -15,7 +15,7 @@ hibernate {
 environments {
     development {
         dataSource {
-			dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+			dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
 			url = "jdbc:mysql://localhost:3306/NewsApp"
 			username = "root"
 			password = "root"
